4187c33b4ccdf6d1d3d2138d0ba0c5a60e760f1c
trhao <1927978923@qq.com>
2025-01-08 22:44:54 +0800

    thread: Uthread passed

--- user/uthread.c
+++ user/uthread.c
@@ -1,7 +1,7 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
+#include "kernel/types.h"
 /* Possible states of a thread: */
 #define FREE        0x0
 #define RUNNING     0x1
@@ -10,15 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
 
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+
+};
 struct thread {
+  struct thread_context context;
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+extern void thread_save(uint64);
 void 
 thread_init(void)
 {
@@ -62,6 +80,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    //  printf("switch\n");
+    thread_switch((uint64)t, (uint64)next_thread);
   } else
     next_thread = 0;
 }
@@ -76,6 +96,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  thread_save((uint64)&t->context);
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack + STACK_SIZE - 1;
 }
 
 void 
